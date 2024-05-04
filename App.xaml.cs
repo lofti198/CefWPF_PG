154a6db46074dc8de9db9e5e4f6878f372873da2
lofti198 <isolar2005@gmail.com>
2024-05-04 16:12:48 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CefWPF_PG;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CefSharp;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CefWPF_PG;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        browser.LoadUrlAsync("https://www.youtube.com/results?search_query=llm");
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        // Clean up CefSharp resources explicitly
+        Cef.Shutdown();
+        base.OnClosed(e);
+    }
+}
